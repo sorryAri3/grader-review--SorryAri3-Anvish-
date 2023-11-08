3eec7df2d45dea9d56aa9040863849c8f893501d
sorryAri3 <144184249+sorryAri3@users.noreply.github.com>
2023-11-07 17:34:26 -0800

    Add files via upload

--- grade.sh
+++ grade.sh
@@ -5,12 +5,59 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
-
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
+git clone $1 student-submission
+echo 'Finished cloning'
+
+## Recieve the files
+submission="student-submission"
+
+if ! [[ -e $submission ]]
+then
+    echo "Error, nothing found"
+    exit
+elif  ! [[ -f $submission/ListExamples.java ]]
+then 
+    echo "Error, no file found"
+    exit
+else
+    echo "Valid file found"
+fi
+
+cp -r $submission/* grading-area
+cp -r *.java grading-area
+cp -r lib grading-area
+cd grading-area
+
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+javac -cp $CPATH *.java
+
+
+testCode=$?
+
+if [[ $testCode -ne 0 ]]
+then 
+    echo $testCode " could not compile"
+    exit
+fi 
+
+java -cp $CPATH org.junit.runner.JUnitCore grading-area/TestListExamples > output.txt
+
+failureString=`grep "FAILURES!!!" output.txt`
+
+if [[ $failureString == "" ]]
+then
+    echo "Passed 100% Yay!"
+else
+    echo "There is a test that has failed :("
+fi
+
+
+
+
+
+
